8393e905a6e1430736cc3d246223f4313779f7a1
prashant4043 <146648038+prashant4043@users.noreply.github.com>
2024-11-01 07:32:16 +0000

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score="33abc"
+
+console.log(typeof score)
+
+let valueInNumber= Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// conversion of string into number datatype but the string which is 33abc cannot be converted into number 
+// therefore it value is NaN but its type is converted into number but not its value.
+
+let bollean=1
+
+let isbolleantrue= Boolean(bollean)
+
+console.log(isbolleantrue);
+
+// 1 = true; 0 = false;
+// "" = false
+// "hello" = true
+
