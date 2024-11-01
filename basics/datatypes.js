ef81eaa4a5a1da76899af1deba8ce44892fadb20
prashant4043 <146648038+prashant4043@users.noreply.github.com>
2024-11-01 07:03:41 +0000

    variables and datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+// alert(3+3) We are using nodejs not any browser lile chrome which has v8 engine in its to execute this alert message as a popup
+
+let name="Prashant"
+
+console.log(typeof "Prashant");
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=231030128
+
+let accountEmail="hello@gmail.com"
+var accountPassword="12345"
+//prefer not to use var in life beacause of its use in scope functionality
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword])
+
