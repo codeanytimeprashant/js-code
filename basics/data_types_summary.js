9781b85b118d8fd8bd19a0524a0edc356a5d4132
prashant4043 <146648038+prashant4043@users.noreply.github.com>
2024-11-15 03:30:58 +0000

    h

---
+++ basics/data_types_summary.js
@@ -0,0 +1,9 @@
+//types of datatypes
+//1. Primitive : 
+//                string, Number, Bollean, null, undefined , symbol, BigInt
+
+
+
+//2. Refrence or non primitive :
+//                            Array, Objects, Functions 
+
