a22480cf250372f5430fce761de503acdc1a64f7
prashant4043 <146648038+prashant4043@users.noreply.github.com>
2024-11-16 13:00:34 +0000

    hlo

--- basics/data_types_summary.js
+++ basics/data_types_summary.js
@@ -7,3 +7,14 @@
 //2. Refrence or non primitive :
 //                            Array, Objects, Functions 
 
+
+//declaration of array 
+const fruits = ["apple","orange","banana"]
+console.log(fruits[0]);
+
+
+//declaration of function
+const myfunction = function(){
+    console.log("hello world");
+    
+}
