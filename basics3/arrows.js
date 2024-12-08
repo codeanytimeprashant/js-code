1f6fcf4643e048a7fc09dca336ff6ddfa3c2829d
prashant4043 <146648038+prashant4043@users.noreply.github.com>
2024-12-08 20:19:12 +0000

    arrows

---
+++ basics3/arrows.js
@@ -0,0 +1,16 @@
+const user = {
+    username: "Prashant",
+    age : 20
+}
+
+
+// const functionName = (parameters) => 
+//   {
+// Function body
+//      };
+
+const addnum = (num1,num2) => ({username : "Prashant"})
+
+console.log(addnum(2,3));
+
+
